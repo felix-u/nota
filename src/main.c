0674f2decc599a45426bbd0907cd348791d0c2c5
felix-u <29074327+felix-u@users.noreply.github.com>
2022-09-13 12:07:47 -0400

    Argument handling

--- src/main.c
+++ src/main.c
@@ -2,7 +2,18 @@
 #include <stdio.h>
 #include <sysexits.h>
 
-int main() {
-    printf("Wassup\n");
+#include "args.c"
+
+
+int main(int argc, char **argv) {
+
+    // @Feature Run on all files in current directory, not just one manually specified file @Feature
+    char **input_arg = (char *[]){"-i", "--input"};
+    char *input_path = args_singleValueOf(argc, argv, input_arg);
+    if (input_path == NULL) {
+        printf("ERROR: Must provide path to file.\n");
+        exit(EX_USAGE);
+    }
+
     return EXIT_SUCCESS;
 }
