df99371fbca0bdae47d257158e7e4311f23ea8ea
felix-u <29074327+felix-u@users.noreply.github.com>
2022-09-21 14:55:39 -0400

    Iterate over file characters, with Unicode support

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sysexits.h>
+#include <wchar.h>
+#include <wctype.h>
+#include <locale.h>
 
 #include "args.c"
 
@@ -8,6 +11,7 @@
 int main(int argc, char **argv) {
 
     // @Feature Run on all files in current directory, not just one manually specified file @Feature
+
     char **input_arg = (char *[]){"-i", "--input"};
     char *input_path = args_singleValueOf(argc, argv, input_arg);
     if (input_path == NULL) {
@@ -15,5 +19,20 @@ int main(int argc, char **argv) {
         exit(EX_USAGE);
     }
 
+    FILE *input_file = fopen(input_path, "r");
+    if (input_file == NULL) {
+        printf("ERROR: Unable to find file \"%s\".\n", input_path);
+        exit(EX_NOINPUT);
+    }
+
+    setlocale(LC_ALL, "");
+    wint_t c;
+    // âñ大
+    while ((c = fgetwc(input_file)) != WEOF) {
+        printf("%lc", c);
+    }
+
+
+    fclose(input_file);
     return EXIT_SUCCESS;
 }
