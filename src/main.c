33077109e74da1a88ee1c9cb847e032016113176
felix-u <29074327+felix-u@users.noreply.github.com>
2022-10-11 01:15:37 -0400

    Need to debug core dumps with valgrind

--- src/main.c
+++ src/main.c
@@ -30,14 +30,7 @@ int main(int argc, char **argv) {
     setlocale(LC_ALL, "");
 
     Node root;
-    wstring name; wstring_init(&name, 1);
-    wstring desc; wstring_init(&desc, 1);
-    wstring date; wstring_init(&date, 1);
-    wstring text; wstring_init(&text, 1);
-    bool getting_name = false;
-    bool getting_desc = false;
-    bool getting_date = false;
-    bool getting_text = false;
+    NodeArray_init(root.children, 1);
 
     wint_t c;
     wchar_t wc;
@@ -45,52 +38,22 @@ int main(int argc, char **argv) {
 
         wc = (wchar_t)c;
 
-        if (getting_name == true) {
-            if (wc == DLM_DESC.beg) {
-                getting_name = false;
-                getting_desc = true;
-            }
-            else if (wc == DLM_DATE.beg) {
-                getting_name = false;
-                getting_date = true;
-            }
-            else if (wc == DLM_TEXT.beg) {
-                getting_name = false;
-                getting_text = true;
-            }
-            else wstring_append(&name, c);
+        if (wc == NODE_MARKER) {
+            NodeArray_append(root.children, Node_process(input_file, &root));
         }
-        else if (getting_desc == true) {
-            if (wc == DLM_DESC.end) getting_desc = false;
-            else wstring_append(&desc, c);
-        }
-        else if (getting_date == true) {
-            if (wc == DLM_DATE.end) getting_date = false;
-            else wstring_append(&date, c);
-        }
-        else if (getting_text == true) {
-            if (wc == DLM_TEXT.end) {
-                getting_text = false;
-                break;
-            }
-            else wstring_append(&text, c);
-        }
-
-        if (wc == NODE_MARKER) getting_name = true;
-        else if (wc == DLM_DESC.beg) getting_desc = true;
-        else if (wc == DLM_DATE.beg) getting_date = true;
-        else if (wc == DLM_TEXT.beg) getting_text = true;
     }
 
-    printf("Name: %ls\n", name.wstr);
-    printf("Desc: %ls\n", desc.wstr);
-    printf("Date: %ls\n", date.wstr);
-    printf("Text: %ls\n", text.wstr);
+    for (size_t i = 0; i < root.children->len; i++) {
+        printf("Name: %ls\n", root.children->nodes[i].name.wstr);
+        printf("Desc: %ls\n", root.children->nodes[i].desc.wstr);
+        printf("Date: %ls\n", root.children->nodes[i].date.wstr);
+        printf("Text: %ls\n", root.children->nodes[i].text.wstr);
+    }
 
-    free(name.wstr);
-    free(desc.wstr);
-    free(date.wstr);
-    free(text.wstr);
+    // free(name.wstr);
+    // free(desc.wstr);
+    // free(date.wstr);
+    // free(text.wstr);
     fclose(input_file);
     return EXIT_SUCCESS;
 }
--- src/node.c
+++ src/node.c
@@ -72,3 +72,67 @@ void NodeArray_append(NodeArray *arr, Node node) {
     }
     arr->nodes[arr->len++] = node;
 }
+
+
+Node Node_process(FILE *file, Node *parent) {
+
+    Node this_node;
+    this_node.parent = parent;
+    wstring_init(&this_node.name, 1);
+    wstring_init(&this_node.desc, 1);
+    wstring_init(&this_node.date, 1);
+    wstring_init(&this_node.text, 1);
+    NodeArray_init(this_node.children, 1);
+
+    bool getting_name = true;
+    bool getting_desc = false;
+    bool getting_date = false;
+    bool getting_text = false;
+
+    wint_t c;
+    wchar_t wc;
+    while ((c = fgetwc(file)) != WEOF) {
+
+        wc = (wchar_t)c;
+
+        if (getting_name == true) {
+            if (wc == DLM_DESC.beg) {
+                getting_name = false;
+                getting_desc = true;
+            }
+            else if (wc == DLM_DATE.beg) {
+                getting_name = false;
+                getting_date = true;
+            }
+            else if (wc == DLM_TEXT.beg) {
+                getting_name = false;
+                getting_text = true;
+            }
+            else wstring_append(&this_node.name, c);
+        }
+        else if (getting_desc == true) {
+            if (wc == DLM_DESC.end) getting_desc = false;
+            else wstring_append(&this_node.desc, c);
+        }
+        else if (getting_date == true) {
+            if (wc == DLM_DATE.end) getting_date = false;
+            else wstring_append(&this_node.date, c);
+        }
+        else if (getting_text == true) {
+            if (wc == DLM_TEXT.end) {
+                getting_text = false;
+                break;
+            }
+            else wstring_append(&this_node.text, c);
+        }
+
+        // if (wc == NODE_MARKER) {
+        //     NodeArray_append(this_node.children, Node_process(file, &this_node));
+        // }
+        if (wc == DLM_DESC.beg) getting_desc = true;
+        else if (wc == DLM_DATE.beg) getting_date = true;
+        else if (wc == DLM_TEXT.beg) getting_text = true;
+    }
+
+    return this_node;
+}
