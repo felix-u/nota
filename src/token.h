eb469049c2190b48c8453e265ba8c736465fa43c
felix-u <29074327+felix-u@users.noreply.github.com>
2023-01-27 19:15:52 -0500

    token.h, testfile

--- src/token.h
+++ src/token.h
@@ -6,11 +6,17 @@
 
 typedef enum token {
     // Single-character syntax
-    T_PAREN_LEFT, T_PAREN_RIGHT, T_SQUARE_BRACKET_LEFT, T_SQUARE_BRACKET_RIGHT, T_COLON, T_SEMICOLON, T_AT,
-    //Keywords
-    T_STR, T_NUM, T_MODS, T_PROVIDES, T_INHERITS,
+    T_PAREN_LEFT, T_PAREN_RIGHT, T_SQUARE_BRACKET_LEFT, T_SQUARE_BRACKET_RIGHT,
+    T_CURLY_BRACKET_LEFT, T_CURLY_BRACKET_RIGHT, T_COLON, T_SEMICOLON, T_AT,
 
-    T_EOF
+    // Types
+    T_STR, T_NUM,
+
+    // Directives
+    T_MODS, T_PROVIDES, T_INHERITS,
+
+    T_EOF,
+    T_COUNT
 } token;
 
 typedef struct token_Array {
