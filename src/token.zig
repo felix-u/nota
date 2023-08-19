54584917ede7150128f1b074411404579bf401ce
felix-u <29074327+felix-u@users.noreply.github.com>
2023-08-19 17:06:41 -0400

    token.zig: fix utf8 handling

--- src/token.zig
+++ src/token.zig
@@ -37,76 +37,83 @@ pub const Token = struct {
 pub fn parseBuf(err_writer: std.fs.File.Writer, set: *parse.Set) !void {
     const allocator = set.allocator;
     const it = &set.buf_it;
+    var last_i = it.i;
 
-    chars: while (it.nextCodepoint()) |c1| switch (c1) {
-        '\r', '\n', '\t', ' ' => {},
-        '{', '}', ':', '=', '.' => {
-            try set.toks.append(allocator, .{
-                .beg_i = @intCast(it.i - 1),
-                .end_i = @intCast(it.i),
-                .kind = set.buf[it.i - 1],
-            });
-        },
-        '/' => {
-            if (it.peek(1)[0] != '/') return log.reportErr(
-                err_writer,
-                Err.InvalidSyntax,
-                set,
-                @intCast(it.i - 1),
-            );
-
-            if (it.nextCodepoint() == null) break :chars;
-            while (it.nextCodepoint()) |c2| if (c2 == '\n') break;
-        },
-        '"' => {
-            const beg_i = it.i + 1;
-            while (it.nextCodepoint()) |c2| {
-                if (it.peek(1)[0] == '\n') return log.reportErr(
+    chars: while (it.nextCodepoint()) |c1| : (last_i = it.i) {
+        switch (c1) {
+            '\r', '\n', '\t', ' ' => {},
+            '{', '}', ':', '=', '.' => {
+                try set.toks.append(allocator, .{
+                    .beg_i = @intCast(last_i),
+                    .end_i = @intCast(it.i),
+                    .kind = set.buf[last_i],
+                });
+            },
+            '/' => {
+                if (set.buf[it.i] != '/') return log.reportErr(
                     err_writer,
-                    Err.NoClosingQuote,
+                    Err.InvalidSyntax,
                     set,
-                    @as(u32, @intCast(it.i)) - 1,
+                    @intCast(last_i),
                 );
 
-                if (it.peek(1)[0] != '"' or c2 == '\\') continue;
+                last_i = it.i;
                 if (it.nextCodepoint() == null) break :chars;
-                break;
-            }
-
-            try set.toks.append(allocator, .{
-                .beg_i = @intCast(beg_i - 1),
-                .end_i = @intCast(it.i - 1),
-                .kind = @intFromEnum(Kind.str),
-            });
-        },
-        else => {
-            if (!parse.isValidSymbolChar(@intCast(c1))) return log.reportErr(
-                err_writer,
-                Err.InvalidSyntax,
-                set,
-                @intCast(it.i - 1),
-            );
-
-            const beg_i: u32 = @intCast(it.i - 1);
-            var this_kind: Kind =
-                if (c1 >= '0' and c1 <= '9') .num else .symbol;
-            if (parse.isValidSymbolChar(it.peek(1)[0])) {
-                while (it.nextCodepoint()) |c2| {
-                    if (c2 == '-') this_kind = .date;
-                    if (!parse.isValidSymbolChar(it.peek(1)[0])) break;
+                while (it.nextCodepoint()) |c2| : (last_i = it.i) {
+                    if (c2 == '\n') break;
                 }
-            }
-            try set.toks.append(allocator, .{
-                .beg_i = beg_i,
-                .end_i = @intCast(it.i),
-                .kind = @intFromEnum(this_kind),
-            });
-        },
-    };
+            },
+            '"' => {
+                const beg_i = it.i + 1;
+                while (it.nextCodepoint()) |c2| : (last_i = it.i) {
+                    if (set.buf[it.i] == '\n') return log.reportErr(
+                        err_writer,
+                        Err.NoClosingQuote,
+                        set,
+                        @as(u32, @intCast(it.i)) - 1,
+                    );
+
+                    if (set.buf[it.i] != '"' or c2 == '\\') continue;
+                    last_i = it.i;
+                    if (it.nextCodepoint() == null) break :chars;
+                    break;
+                }
+
+                try set.toks.append(allocator, .{
+                    .beg_i = @intCast(beg_i - 1),
+                    .end_i = @intCast(last_i),
+                    .kind = @intFromEnum(Kind.str),
+                });
+            },
+            else => {
+                if (!parse.isValidSymbolChar(@intCast(c1))) return log.reportErr(
+                    err_writer,
+                    Err.InvalidSyntax,
+                    set,
+                    @intCast(last_i),
+                );
+
+                const beg_i: u32 = @intCast(last_i);
+                var this_kind: Kind =
+                    if (c1 >= '0' and c1 <= '9') .num else .symbol;
+                if (parse.isValidSymbolChar(it.peek(1)[0])) {
+                    while (it.nextCodepoint()) |c2| : (last_i = it.i) {
+                        if (c2 == '-') this_kind = .date;
+                        if (!parse.isValidSymbolChar(it.peek(1)[0])) break;
+                    }
+                }
+                try set.toks.append(allocator, .{
+                    .beg_i = beg_i,
+                    .end_i = @intCast(it.i),
+                    .kind = @intFromEnum(this_kind),
+                });
+            },
+        }
+    }
 
     try set.toks.append(allocator, .{
-        .beg_i = if (it.i > 0) @intCast(it.i - 1) else 0,
-        .end_i = if (it.i > 0) @intCast(it.i - 1) else 0,
+        .beg_i = if (it.i > 0) @intCast(set.buf.len - 1) else 0,
+        .end_i = if (it.i > 0) @intCast(set.buf.len - 1) else 0,
         .kind = @intFromEnum(Kind.eof),
     });
 }
